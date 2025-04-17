71b3002e6b9d165f44058d1a158e29b5c39b3a9b
daniel-duke <dhd13@duke.edu>
2025-04-17 16:39:15 -0400

    hopefully now cadnano works

--- source/source/conf.py
+++ source/source/conf.py
@@ -17,13 +17,11 @@ release = '2024'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['myst_parser']
-
 templates_path = ['_templates']
-exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_titla = 'DNAfold Documentation'
